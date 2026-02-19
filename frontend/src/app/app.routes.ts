735f43a0591a8fd43a5034f0a5da5a246fc24a66
daniieldvir <daniieldvir2406@gmail.com>
2026-02-19 12:12:20 +0200

    Updates

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AnalyzerPage } from './pages/analyzer-page/analyzer-page';
-
-export const routes: Routes = [
-    {
-      path: '',
-      component: AnalyzerPage
-    }
-  ];
